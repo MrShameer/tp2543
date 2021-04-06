49581863ff603ffa64fd76de81bd1ba593edc825
MrShameer <67554400+MrShameer@users.noreply.github.com>
2021-04-07 00:51:01 +0800

    Create land.js

---
+++ land.js
@@ -0,0 +1,13 @@
+function box(a,b){
+	let to = ['<div class="box">',
+				'<span></span>',
+				'<div class="content">',
+				'<h2>'+a+'</h2>',
+				'<a href="+'b'+.html">+'a'+</a>',
+				'</div>',
+				'</div>']
+
+	return to.join('\n');
+}
+
+
