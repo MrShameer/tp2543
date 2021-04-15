37b95922878a86340a33b2e4cf487210cadeef5b
MrShameer <67554400+MrShameer@users.noreply.github.com>
2021-04-15 17:17:22 +0800

    .

---
+++ assignment/guessbook/edit.php
@@ -0,0 +1,78 @@
+<?php
+ 
+if (isset($_GET['id'])) {
+ 
+  include "db.php";
+ 
+  try {
+      $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+       
+      $stmt = $conn->prepare("SELECT * FROM assignment2 WHERE id = :record_id");
+      $stmt->bindParam(':record_id', $id, PDO::PARAM_INT);
+      $id = $_GET['id'];
+ 
+      $stmt->execute();
+      $result = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+      }
+ 
+    catch(PDOException $e)
+    {
+        echo "Error: " . $e->getMessage();
+    }
+ 
+    $conn = null;
+  }
+else {
+  echo "Error: You have execute a wrong PHP. Please contact the web administrator.";
+  die();
+}
+ 
+ ?>
+ 
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Guestbook</title>
+</head>
+ 
+<body>
+ 
+<form method="post" action="update.php">
+  Nama :
+  <input type="text" name="name" size="40" required value="<?php echo $result["user"]; ?>">
+  <br>
+  Email :
+  <input type="text" name="email" size="25" required value="<?php echo $result["email"]; ?>">
+  <br>
+
+  How did you find me? 
+  <select name="find" id="find">
+    <option value="From a friend" required>From a friend</option>
+    <option value="I google you" required>I google you</option>
+    <option value="Just surf on in" required>Just surf on in</option>
+    <option value="From your facebook" required>From your facebook</option>
+    <option value="I clicked on ads" required>I clicked on ads</option>
+  </select>
+  <br>
+  I like your :<br>
+  <input type="checkbox" id="tick1" name="tick[]" value="frontpage" <?php echo (strpos($result["tick"], 'frontpage')===0 ? 'checked' : '');?>>
+  <label for="tick1">Front Page</label><br>
+  <input type="checkbox" id="tick2" name="tick[]" value="form" <?php echo (strpos($result["tick"], 'form')===0 ? 'checked' : '');?>>
+  <label for="tick2">Form</label><br>
+  <input type="checkbox" id="tick3" name="tick[]" value="userinterface" <?php echo (strpos($result["tick"], 'userinterface')===1 ? 'checked' : '');?>>
+  <label for="tick3">User Interface</label><br><br>
+
+
+  Comments :<br>
+  <textarea name="comment" cols="30" rows="8" required><?php echo $result["comment"]; ?></textarea>
+  <br>
+  <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
+  <input type="submit" name="edit_form" value="Modify Comment">
+  <input type="reset">
+  <br>
+</form>
+ 
+</body>
+</html>
---
+++ assignment/guessbook/form.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Guestbook</title>
+</head>
+ 
+<body>
+ 
+<form method="post" action="insert.php">
+	Nama :
+	<input type="text" name="name" size="40" required>
+	<br>
+	Email :
+	<input type="text" name="email" size="25" required>
+	<br>
+	How did you find me? 
+	<select name="find" id="find">
+		<option value="From a friend" required>From a friend</option>
+		<option value="I google you" required>I google you</option>
+		<option value="Just surf on in" required>Just surf on in</option>
+		<option value="From your facebook" required>From your facebook</option>
+		<option value="I clicked on ads" required>I clicked on ads</option>
+	</select>
+	<br>
+	I like your :<br>
+	<input type="checkbox" id="tick1" name="tick[]" value="frontpage">
+  <label for="tick1">Front Page</label><br>
+  <input type="checkbox" id="tick2" name="tick[]" value="form">
+  <label for="tick2">Form</label><br>
+  <input type="checkbox" id="tick3" name="tick[]" value="userinterface">
+  <label for="tick3">User Interface</label><br><br>
+	Comments :<br>
+	<textarea name="comment" cols="30" rows="8" required></textarea>
+	<br>
+	<input type="submit" name="add_form" value="Add a New Comment">
+	<input type="reset">
+	<br>
+</form>
+ 
+</body>
+</html>
