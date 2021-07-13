1a25eecc429fbcd98fc3d38d0394022cfbbeaf17
MrShameer <67554400+MrShameer@users.noreply.github.com>
2021-07-14 04:19:34 +0800

    .

--- mypt4/nav_bar.php
+++ mypt4/nav_bar.php
@@ -1,10 +1,12 @@
+<?php
+include_once 'database.php';
+?>
 <link rel="shortcut icon" type="image/x-icon" href="products/hypers logo.ico"/>
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <link href="style.css" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <nav class="navbar navbar-default">
 	<div class="container-fluid">
-		<!-- Brand and toggle get grouped for better mobile display -->
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
 				<span class="sr-only">Toggle navigation</span>
@@ -14,18 +16,17 @@
 			</button>
 			<a class="navbar-brand" href="./">Hypers</a>
 		</div>
- 
-		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="navbar-brand" id="role"><?php echo $_SESSION['user']['fld_staff_role'].' : '.$_SESSION['user']['fld_staff_name']?></div>
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		<ul class="nav navbar-nav navbar-right">
-			<li><a href="products.php">Products</a></li>
-			<li><a href="customers.php">Customers</a></li>
-			<li><a href="staffs.php">Staffs</a></li>
-			<li><a href="orders.php">Orders</a></li>
-			<li><a href="logout.php" data-toggle="tooltip" id="out"><span class="glyphicon glyphicon-log-out"></span></a></li>
-		</ul>
-		</div><!-- /.navbar-collapse -->
-	</div><!-- /.container-fluid -->
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="products.php">Products</a></li>
+				<li><a href="customers.php">Customers</a></li>
+				<li><a href="staffs.php">Staffs</a></li>
+				<li><a href="orders.php">Orders</a></li>
+				<li><a href="logout.php" data-toggle="tooltip" id="out"><span class="glyphicon glyphicon-log-out"></span></a></li>
+			</ul>
+		</div>
+	</div>
 </nav>
 <script type="text/javascript">
 	$(function(){
